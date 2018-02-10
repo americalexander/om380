ef791453944f6e6da4f6a55d4c321f8484fecc6f
William E. Alexander <williamericalexander@gmail.com>
2018-02-10 08:53:13 -0600

    Q1 done, minor adjustments

--- ArcAdjacency.py
+++ ArcAdjacency.py
@@ -1,8 +1,12 @@
 class ArcAdjacencyList:
 	def __init__(self):
 		self.iNodes = dict()
+		self.jNodes = dict()
 	
 	def addArc(self, arc):
 		if self.iNodes.get(arc.i) == None:
 			self.iNodes[arc.i] = []
+		if self.jNodes.get(arc.j) == None:
+			self.jNodes[arc.j] = []
 		self.iNodes.get(arc.i).append(arc)
+		self.jNodes.get(arc.j).append(arc)
--- Assignment1.py
+++ Assignment1.py
@@ -2,9 +2,16 @@
 from ArcReader import ArcReader
 import sys
 import time
+import queue
 
-def q1():
-	pass
+def q1(list):
+	q = queue.PriorityQueue()
+	for node in list.jNodes.keys():
+		q.put((-len(list.jNodes[node]),node))
+	maxTrust = []
+	for i in range(20):
+		maxTrust.append(q.get())
+	return maxTrust
 
 def q2():
 	pass
@@ -25,21 +32,24 @@ def main():
 	
 	## Question 1
 	t0 = time.perf_counter()
-	q1()
+	maxTrust = q1(list)
 	t1 = time.perf_counter()
-	print("Q1 Time: %s\tseconds" % str(t1-t0))
+	print("MOST TRUSTED:")
+	for tuple in maxTrust:
+		print("Individual "+str(tuple[1]))
+	print("Q1 Time: %s\tseconds\n" % str(t1-t0))
 	
 	## Question 2
 	t1 = time.perf_counter()
 	q2()
 	t2 = time.perf_counter()
-	print("Q2 Time: %s\tseconds" % str(t2-t1))
+	print("Q2 Time: %s\tseconds\n" % str(t2-t1))
 	
 	## Question 3
 	t2 = time.perf_counter()
 	q3()
 	t3 = time.perf_counter()
-	print("Q3 Time: %s\tseconds" % str(t3-t2))
+	print("Q3 Time: %s\tseconds\n" % str(t3-t2))
 	
 	## Question 4
 	t3 = time.perf_counter()
